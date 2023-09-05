d76e8390c6a9ff3202759df4ab72e5aee03b2b99
telecomshy <telecomshy@189.cn>
2023-09-05 20:48:30 +0800

    添加创建，修改，删除文件夹

---
+++ imapbox/tests/test_fake_box/conftest.py
@@ -0,0 +1,33 @@
+import pytest
+from imaplib import IMAP4_SSL
+from imapbox import MailBox
+
+
+class FakeImap(IMAP4_SSL):
+
+    def list(self, *args, **kwargs):
+        return 'OK', [b'(\\Marked) "/" "INBOX"', b'(\\Marked) "/" "Drafts"', b'(\\Marked) "/" "&XfJT0ZAB-"']
+
+    def _connect(self, *args, **kwargs):
+        ...
+
+    def shutdown(self, *args, **kwargs):
+        ...
+
+    def open(self, *args, **kwargs):
+        ...
+
+    def login(self, *args, **kwargs):
+        return 'OK', [b'LOGIN completed']
+
+    def logout(self, *args, **kwargs):
+        ...
+
+
+@pytest.fixture(scope='session')
+def fake_box():
+    box = MailBox('imap.fakeserver.com')
+    box.imap_cls = FakeImap
+    box.login()
+    yield box
+    box.quit()
